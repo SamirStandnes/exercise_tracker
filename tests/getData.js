ef95edb31f7192453192d839ece7771135ad8885
Samir Standnes <samir.standnes@lanekassen.no>
2019-05-01 15:10:44 +0200

    save progress on getData middleware

--- tests/getData.js
+++ tests/getData.js
@@ -5,6 +5,17 @@ const db = require("../models/db");
 router.get('/api/exercise/log?', function (req, res) {
     console.log('GET data from userId');
     console.log(req.query.userId);
+    
+    let userId = req.query.userId;
+    let fromDate = (req.query.from === null ? null : Date(req.query.from));
+    let toDate = Date(req.query.to);    
+    let limit = Number(req.query.limit);
+    
+    console.log(userId, fromDate, toDate, limit);
+    
+    // function to filter the log array based on query parameters
+    //
+
     /*
     db.User.findOne({ user_id: req.query.userId }, function (err, doc) {
         if (err) {
