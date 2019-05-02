8a346978e6df86e66948b524962cb97615c4b2db
Samir Standnes <samir.standnes@lanekassen.no>
2019-05-02 11:41:41 +0200

    save progress on getData feature

--- tests/getData.js
+++ tests/getData.js
@@ -2,21 +2,44 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/db");
 
-router.get('/api/exercise/log?', function (req, res) {
-    console.log('GET data from userId');
-    console.log(req.query.userId);
-    
-    let userId = req.query.userId;
-    let fromDate = (req.query.from === null ? null : Date(req.query.from));
-    let toDate = Date(req.query.to);    
-    let limit = Number(req.query.limit);
-    
-    console.log(userId, fromDate, toDate, limit);
-    
-    // function to filter the log array based on query parameters
-    //
+/*
+const filterFunction = (done, userId, limit) => {
+  db.User.find({ user_id: userId })
+    .limit(limit)
+    .exec(function(err, data) {
+      if (err) {
+        return done(err);
+      }
+      console.log(data);
+      return done(null, data);
+    });
+};
+*/
 
-    /*
+router.get("/api/exercise/log?", function(req, res) {
+  console.log("GET data from userId");
+  console.log(req.query.userId);
+
+  let userId = String(req.query.userId);
+  let fromDate = req.query.from;
+  //(req.query.from === undefined ? null : Date(req.query.from));
+  let toDate = req.query.to;
+  let limit = req.query.limit;
+
+  db.User.findOne({ user_id: userId }).exec(function(err, doc) {
+      if (err) {
+        throw err;
+      }
+      console.log(doc.exercise);
+      //res.json({status: doc});
+    });
+
+  console.log(userId, fromDate, toDate, limit);
+
+  // function to filter the log array based on query parameters
+  //
+
+  /*
     db.User.findOne({ user_id: req.query.userId }, function (err, doc) {
         if (err) {
             console.log("some error in updating");
